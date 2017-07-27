acc82c78b4ffd787edcc82286866926f5f7bd3f7
Svo Hurts <svoharts@gmail.com>
2017-07-27 19:06:06 +0300

    for scotov

--- e_check/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ECheckConfig(AppConfig):
-    name = 'e_check'
--- echeck/urls.py
+++ echeck/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include ,url
 from django.contrib import admin
+from cashier.views import cashier, add_dish_to_order
 
 urlpatterns = [
+
+
+	#e_check urls
+	url(r'^cashier/(?P<order_id>[0-9]+)/$', cashier, name = 'cashier'),
+    url(r'^cashier/add_to_order/(?P<dish_id>\d+)/$', add_dish_to_order, name = 'add_dish_to_order'),
+
+
+
+    #cashier urls
+    #url(r'^storage/$'),
+
+    
     url(r'^admin/', admin.site.urls),
 ]
